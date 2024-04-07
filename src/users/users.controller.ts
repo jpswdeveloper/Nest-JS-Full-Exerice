94428ea43e0b2a1aa63a3ea9a90095572e5f3a56
Tedy Girma <girmatedy48@gmail.com>
2024-04-07 12:11:03 +0300

    handle all http routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // :: user route (includes all related to user route like get all routes,get by id)
+export class UsersController {
+  /**
+   * Include http methods for user route
+   * User controller handles user information || user route in express
+   */
+
+  @Get()
+  findAll(@Query('page') page?: number, @Query('limit') limit?: number) {
+    // Get all users
+    return {
+      query: { page, limit },
+      data: [],
+    };
+  }
+
+  @Get('interns')
+  findAllInterns() {
+    return [];
+  }
+
+  @Post('ted/:id')
+  create(
+    @Body() parameters: {},
+    @Param('id') id: String,
+    @Query('all') all: { page: number; limit: 1 },
+  ) {
+    console.log('all', all);
+    return { id, parameters, all };
+  }
+
+  @Get(':id')
+  GetInfo(@Param('id') id: string) {
+    return id;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return id;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return { id };
+  }
+}
