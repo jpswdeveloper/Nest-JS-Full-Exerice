82df81f58870974d3e3b258025e2dccb6bd63fae
Tedy Girma <girmatedy48@gmail.com>
2024-04-07 11:45:31 +0300

    creating controller,service ,providers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {} // new service  || extend service class
 
   @Get()
   getHello(): string {
